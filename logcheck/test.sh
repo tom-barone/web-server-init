da39d689f127b65db3dc03cf33638143f0afed65
Tom Barone <mail@tombarone.net>
2025-02-19 16:13:32 +1030

    Linting fix

--- logcheck/test.sh
+++ logcheck/test.sh
@@ -3,8 +3,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
 
 temp_file=$(mktemp)
-# Remove comments and empty lines from the log file and em
-cat test_log.txt | grep -v "^#.*$" | grep -v "^\s*$" >"$temp_file"
+# Remove comments and empty lines from the log file
+grep -v "^#.*$" test_log.txt | grep -v "^\s*$" >"$temp_file"
 
 # Process each logcheck regex file
 for logfile in rules/*; do
